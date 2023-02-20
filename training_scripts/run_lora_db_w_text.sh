2c53e2ae759a8441cb8c7087087da5d4d96a9577
996477559@qq.com <996477559@qq.com>
2023-02-20 17:18:35 +0800

    add lora_rank

--- training_scripts/run_lora_db_w_text.sh
+++ training_scripts/run_lora_db_w_text.sh
@@ -13,11 +13,12 @@ accelerate launch train_lora_dreambooth.py \
   --instance_prompt="a lyc person" \
   --train_text_encoder \
   --resolution=512 \
-  --train_batch_size=1 \
+  --train_batch_size=4 \
   --gradient_accumulation_steps=1 \
   --learning_rate=1e-4 \
   --learning_rate_text=5e-5 \
   --color_jitter \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=1000
+  --max_train_steps=1000 \
+  --lora_rank=96
